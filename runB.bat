e81f3404b1a0ecfa29ce76f834b76096e8bed9a3
Brendson <brendson.net@gmail.com>
2022-09-20 13:58:05 -0300

    feat: criando scripts para inciar as aplicações

---
+++ runB.bat
@@ -0,0 +1,7 @@
+@Echo Off 
+@Echo. 
+@Echo ======== iniciando back ========
+@Echo. 
+
+cd backend
+./mvnw spring-boot:run
---
+++ runF.bat
@@ -0,0 +1,7 @@
+@Echo Off 
+@Echo. 
+@Echo ======== iniciando font ========
+@Echo. 
+
+cd frontend
+npm run serve
