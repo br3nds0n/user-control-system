0e058945fb7ac8f8b7059c0d674d62f383914398
Brendson <brendson.net@gmail.com>
2022-09-28 18:01:21 -0300

    feat: encriptando senha de usuário

--- backend/src/main/java/br/com/api/system/config/WebSecurityConfig.java
+++ backend/src/main/java/br/com/api/system/config/WebSecurityConfig.java
@@ -5,10 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPassword() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
