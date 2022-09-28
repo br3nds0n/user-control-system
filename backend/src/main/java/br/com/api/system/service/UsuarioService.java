89df04d75794dd6e558e8c4c7660feb4f0d25b11
Brendson <brendson.net@gmail.com>
2022-09-28 15:41:51 -0300

    feat: criando rota para criar usuário

---
+++ backend/src/main/java/br/com/api/system/service/UsuarioService.java
@@ -0,0 +1,39 @@
+package br.com.api.system.service;
+
+import br.com.api.system.DTO.CepDTO;
+import br.com.api.system.api.ViaCepConsumer;
+import br.com.api.system.exception.BadRequestException;
+import br.com.api.system.model.Usuario;
+import br.com.api.system.repository.UsuarioRepository;
+import br.com.api.system.utils.CpfUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Autowired
+    private ViaCepConsumer viaCepConsumer;
+
+    public Usuario criar(Usuario usuario) {
+        if (!CpfUtil.isCPF(usuario.getCpf())) throw new BadRequestException("Cpf inválido");
+
+        CepDTO response = this.viaCepConsumer.consultarCep(usuario.getCep());
+
+        usuario.setUf(response.getUf());
+        usuario.setBairro(response.getBairro());
+        usuario.setLocalidade(response.getLocalidade());
+        usuario.setLogradouro(response.getLogradouro());
+
+        usuario.setCpf(CpfUtil.formatarCPF(usuario.getCpf()));
+        usuario.setCriado(LocalDateTime.now());
+        usuario.setModificado(LocalDateTime.now());
+
+        return this.repository.save(usuario);
+    }
+}
