2f6e3e190df3092e09b5e9a588c44328b1ddfe20
Brendson <brendson.net@gmail.com>
2022-09-30 11:28:49 -0300

    feat: realizando validações do formulário e respostas de erro

--- frontend/src/app/auth/LoginComponent.vue
+++ frontend/src/app/auth/LoginComponent.vue
@@ -17,23 +17,25 @@
                 <span class="p-inputgroup-addon">
                   <i class="pi pi-envelope"></i>
                 </span>
-                <InputText v-model="email" placeholder="E-mail" />
+                <InputText v-model="email"  @input="v$.email.$touch()" placeholder="E-mail" />
               </div>
+              <small class="p-error" v-if="v$.email.$error">E-mail é obrigatório</small>
             </div>
             <div class="field col-12 mt-2">
               <div class="p-inputgroup">
                 <span class="p-inputgroup-addon">
                   <i class="pi pi-key"></i>
                 </span>
-                <InputText v-model="senha" type="password" placeholder="Senha" />
+                <InputText v-model="senha" type="password" @input="v$.senha.$touch()" placeholder="Senha" />
               </div>
+              <small class="p-error" v-if="v$.senha.$error">Senha é obrigatória</small>
             </div>
           </div>
           <div class="col-12 d-inline-flex flex-column justify-content-center mt-2">
             <Button
               label="Entrar"
               icon="pi pi-check"
-              v-on:click="login"
+              @click="send"
               class="btnLogin"
             />
             <div class="mt-4 text-center">
@@ -48,27 +50,47 @@
 
 <script>
 import service from './service'
+import useVuelidate from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
 
 export default {
+  setup () {
+    return { v$: useVuelidate() }
+  },
   data () {
     return {
-      email: null,
-      senha: null
+      email: '',
+      senha: ''
+    }
+  },
+  validations () {
+    return {
+      email: { required },
+      senha: { required }
     }
   },
   methods: {
+    send () {
+      this.v$.$touch()
+      if (this.v$.$invalid) return
+      this.$store.dispatch('addRequest')
+      this.login()
+    },
+
     login () {
       const credenciais = {
         email: this.email,
         senha: this.senha
       }
 
-      this.$store.dispatch('addRequest')
       service.login(credenciais).then((res) => {
         if (res.data.token != null) {
           localStorage.setItem('token', res.data.token)
           this.$router.push('/')
         }
+        this.$store.dispatch('removeRequest')
+      }).catch((e) => {
+        this.$toast.add({ severity: 'error', summary: 'Error', detail: `${e.response.data.descricao}`, life: 3000 })
         this.$store.dispatch('removeRequest')
       })
     }
